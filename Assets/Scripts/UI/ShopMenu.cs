b8bada8f49957e358780e9e4701bd0e624bd7561
OhRidgeInAll <8bitOriginal@gmail.com>
2025-11-06 13:30:30 -0500

    Created base menu to edit from
    Includes functional exit button that restores input to player through campmanager function

--- Assets/Scripts/UI/ShopMenu.cs
+++ Assets/Scripts/UI/ShopMenu.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ShopMenu : MonoBehaviour, IGameMenu
 {
@@ -6,10 +8,15 @@ public class ShopMenu : MonoBehaviour, IGameMenu
     public bool IsOpen { get; private set; }
     public bool escapable { get; private set; }
 
+    public Button enterButton;
+    public Button talkButton;
+    public Button exitButton;
+
     public void Awake()
     {
         escapable = true;
         IsOpen = false;
+        exitButton.onClick.AddListener(OnExit);
     }
 
     public void Open()
@@ -26,5 +33,9 @@ public class ShopMenu : MonoBehaviour, IGameMenu
         Debug.Log("ShopMenu.cs: Shop menu closed");
     }
 
-    // button hooks go here
+    private void OnExit()
+    {
+        Close();
+        CampInputBlocker.SetBlocked(false);
+    }
 }
